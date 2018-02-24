3e0abe3f5ec7cc76783198b3953bec7c3e2d4810
BrenthGH-YT <36789825+BrenthGH-YT@users.noreply.github.com>
2018-02-24 06:47:38 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -37,7 +37,7 @@ client.on('message', message => {
 
 client.on('message', message => {
     if (message.content === 'g<help') {
-    	message.reply('1. g<info 2. g<ping 3. g<hello 4. g<hi 5. g<creator 6. g<discord 7. g<github 8. g<tools 9. g<bing 10. g<xd 11. g<help 12. g<uganda 13. g<friends');
+    	message.reply('1. g<info 2. g<ping 3. g<hello 4. g<hi 5. g<creator 6. g<discord 7. g<github 8. g<tools 9. g<bing 10. g<xd 11. g<help 12. g<uganda 13. g<friends 14. g<memes 15. g<mcgods 16. g<xblazerpk 17. g<download 18. g<mcpros');
   	}
 });
 
@@ -80,6 +80,36 @@ client.on('message', message => {
 client.on('message', message => {
     if (message.content === 'g<friends') {
     	message.reply('Creators Friends: MC Gods#2808 NICO_THE_PRO#5337 xBlazerPK#9165 ShqneFNM#7404');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'g<memes') {
+    	message.reply('https://orig00.deviantart.net/e70c/f/2018/017/1/0/ugandan_knuckles_by_imprimez-dc0cj6c.png https://ih1.redbubble.net/image.32625229.2652/flat,800x800,070,f.jpg https://i.amz.mshcdn.com/GW_Be6gnFzS2rtF-KJ1CoIbsY0s=/1200x627/2016%2F02%2F06%2F55%2FGettyImages.0e07c.jpg https://i.ytimg.com/vi/Ou6uXU0KFss/hqdefault.jpg https://www.funnypica.com/wp-content/uploads/2015/05/Funny-Memes-17-570x641.jpg https://i.pinimg.com/736x/d6/09/e2/d609e22384ddd556414b27644053dea3.jpg');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'g<mcgods') {
+    	message.reply('MC Gods#2808: Imaginary playing GMOD Prop Hunt! Me: Yey? MC Gods#2808: Ooh! Good hiding spot is this the secrets of youtube. Me: Secrets of youtube? you think they now whats going on. MC Gods#2808: has been crushed to death.');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'g<xblazerpk') {
+    	message.reply('xBlazerPK: dude im going to win bedwars i tell ya! Your dick is going to be wonged if i kill you this round. Me: Talking about my dick and being a dick doesnt make yours any bigger. xBlazerPK: fell into the void.');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'g<download') {
+    	message.reply('https://discordapp.com/oauth2/authorize?client_id=369375342466433025&scope=bot&permissions=0');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'g<mcpros') {
+    	message.reply('Pro: Sup! Where are you hiding? Im going to kill you (bedwars) and if I win i raise my mid finger to my victory. Me: Roses are Red, Violets are Blue, I have five fingers the middle ones for you. Pro: fell into the void.');
   	}
 });
 
